59f53d0404a7e1598f99f00e0e3da82406b32239
MxBromelia <ruanmacambira@gmail.com>
2020-06-09 01:39:48 -0300

    report gera output de colunas

--- lib/report.py
+++ lib/report.py
@@ -32,9 +32,17 @@ class TableReport:
         self.table_name = table_name
         self.messages = messages
 
+class ColumnReport:
+    def __init__(self, table_name: str, column_name: str, messages: List[str]):
+        self.table_name = table_name
+        self.column_name = column_name
+        self.messages = messages
+
 class Report:
-    def __init__(self, table_reports: List[TableReport] = None):
-        self.table_reports = table_reports if table_reports else []
+    def __init__(self, table_reports: List[TableReport] = None,
+                 column_reports: List[ColumnReport] = None):
+        self.table_reports: List[TableReport] = table_reports if table_reports else []
+        self.column_reports: List[ColumnReport] = column_reports if column_reports else []
 
 def generate_table_report(table_report: TableReport):
     output = []
@@ -42,18 +50,36 @@ def generate_table_report(table_report: TableReport):
 
     for message in table_report.messages:
         output.append(f'   + {message}')
-    
+
+    return output
+
+def generate_column_report(column_report: ColumnReport):
+    output = []
+    output.append(f' + {column_report.table_name}.{column_report.column_name}')
+    for message in column_report.messages:
+        output.append(f'   + {message}')
+
     return output
 
 def generate_report(report):
     output = []
     output.append('REPORT')
     output.append('=' * 50)
-    output.append('Tables:')
-    output.append('=' * 50)
+
+    if len(report.table_reports) > 0:
+        output.append('Tables:')
+        output.append('=' * 50)
 
     for table_report in report.table_reports:
         output += generate_table_report(table_report)
+        output.append('-' * 40)
+
+    if len(report.column_reports) > 0:
+        output.append('Columns:')
+        output.append('=' * 50)
+
+    for column_report in report.column_reports:
+        output += generate_column_report(column_report)
         output.append('-' * 40)
 
     return output
--- test/report_test.py
+++ test/report_test.py
@@ -3,11 +3,31 @@
 
 from lib.report import Report, generate_report
 from lib.report import TableReport, generate_table_report
+from lib.report import ColumnReport, generate_column_report
 
-def test_generate_table_report():
+def test_generate_table_report_one_message():
     table_report = TableReport(table_name='table_one', messages=['message one'])
 
     assert generate_table_report(table_report) == [' + table_one', '   + message one']
+
+def test_generate_table_report_two_messages():
+    table_report = TableReport(table_name='table_one', messages=['message one', 'message two'])
+
+    assert generate_table_report(table_report) == \
+        [' + table_one', '   + message one', '   + message two']
+
+def test_generate_column_report_one_message():
+    column_report = ColumnReport(table_name='table_one',
+                                 column_name='column_one', messages=['message one'])
+
+    assert generate_column_report(column_report) == [' + table_one.column_one', '   + message one']
+
+def test_generate_column_report_two_messages():
+    column_report = ColumnReport(table_name='table_one',
+                                 column_name='column_one', messages=['message one', 'message two'])
+
+    assert generate_column_report(column_report) == \
+        [' + table_one.column_one', '   + message one', '   + message two']
 
 def test_generate_report_one_table():
     table_report = TableReport(table_name='table_one', messages=['message one'])
@@ -29,4 +49,43 @@ def test_generate_report_two_tables():
         'REPORT', '=' * 50, 'Tables:', '=' * 50,
         ' + table_one', '   + message one', '-' * 40,
         ' + table_two', '   + message two', '-' * 40
+    ]
+
+def test_generate_report_one_column():
+    column_reports = [
+        ColumnReport(table_name='table_one', column_name='column_one', messages=['message one'])
+    ]
+    report = Report(column_reports=column_reports)
+
+    assert generate_report(report) == [
+        'REPORT', '=' * 50, 'Columns:', '=' * 50,
+        ' + table_one.column_one', '   + message one', '-' * 40
+    ]
+
+def test_generate_report_two_columns():
+    column_reports = [
+        ColumnReport(table_name='table_one', column_name='column_one', messages=['message one']),
+        ColumnReport(table_name='table_one', column_name='column_two', messages=['message two'])
+    ]
+    report = Report(column_reports=column_reports)
+
+    assert generate_report(report) == [
+        'REPORT', '=' * 50, 'Columns:', '=' * 50,
+        ' + table_one.column_one', '   + message one', '-' * 40,
+        ' + table_one.column_two', '   + message two', '-' * 40
+    ]
+
+def test_generate_report_one_table_one_column():
+    table_reports = [TableReport(table_name='table_one', messages=['message one'])]
+    column_reports = [
+        ColumnReport(table_name='table_one', column_name='column_one', messages=['message one'])
+    ]
+    report = Report(table_reports=table_reports, column_reports=column_reports)
+
+    assert generate_report(report) == [
+        'REPORT', '=' * 50,
+        'Tables:', '=' * 50,
+        ' + table_one', '   + message one', '-' * 40,
+        'Columns:', '=' * 50,
+        ' + table_one.column_one', '   + message one', '-' * 40
     ]
