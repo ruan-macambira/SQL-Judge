9ba338a8b6737db129f3d42c9c307b5d5ab16e97
MxBromelia <ruanmacambira@gmail.com>
2020-06-09 22:37:23 -0300

    input sanittization

--- lib/generate_schema.py
+++ lib/generate_schema.py
@@ -8,13 +8,14 @@ def generate_schema(conn: DBConnection) -> Schema:
         add_table(schema, table)
 
         for column_hash in conn.columns(table_name):
-            is_primary_key = column_hash['primary_key'] if 'primary_key' in column_hash else 'false'
-            column = Column(name=column_hash['name'],
-                            col_type=column_hash['type'],
-                            primary_key=as_bool(is_primary_key))
+            column = Column(name=column_hash['name'], col_type=column_hash['type'],
+                            primary_key=as_bool(sanitize(column_hash, 'primary_key', 'false')))
             add_column(table, column)
 
     return schema
+
+def sanitize(unsanitized_hash: dict, key, default=None):
+    return unsanitized_hash[key] if key in unsanitized_hash else default
 
 def as_bool(not_bool: str) -> bool:
     return {'false': False, 'true': True}[not_bool]
