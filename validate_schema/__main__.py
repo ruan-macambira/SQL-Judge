7d0a18c47ded489188dceda4eadd70aff4d8364b
MxBromelia <ruanmacambira@gmail.com>
2020-08-31 23:23:11 -0300

    validador aceita mais de um arquivo de configuração

--- validate_schema/__main__.py
+++ validate_schema/__main__.py
@@ -6,22 +6,24 @@ from .run import run
 from .parse_configuration.build_configuration import ConfigurationBuilder
 
 def default_config():
-    return pkg_resources.resource_string('validate_schema.parse_configuration', 'default_configuration.json')
+    return pkg_resources.resource_string(
+        'validate_schema.parse_configuration', 'default_configuration.json')
 
 def user_config(filename):
-    with open(filename) as f:
-        return f.read(None)
+    with open(filename) as file:
+        return file.read(None)
 
-def validate_schema(filename):
+def validate_schema(filenames):
     """ Main function """
-    config = ConfigurationBuilder.from_json(default_config()) \
-        .merge(ConfigurationBuilder.from_json(user_config(filename))) \
-        .build()    
+    config_builder = ConfigurationBuilder.from_json(default_config())
+    for filename in filenames:
+        config_builder = config_builder.merge(ConfigurationBuilder.from_json(user_config(filename)))
+    config = config_builder.build()
 
     return run(config)
 
 if __name__ == '__main__':
-    report = validate_schema(sys.argv[1])
+    report = validate_schema(sys.argv[1:])
 
     for line in report:
         print(line)
