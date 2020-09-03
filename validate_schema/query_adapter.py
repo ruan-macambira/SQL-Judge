b5bd2575154c2b1dba9baaaa3c133169f2c53f15
MxBromelia <ruanmacambira@gmail.com>
2020-09-03 17:48:28 -0300

    QueryAdapter faz eager-loading dos resultados das queries

--- validate_schema/query_adapter.py
+++ validate_schema/query_adapter.py
@@ -2,37 +2,46 @@ from typing import List, Dict, Optional
 from .adapter import DBAdapter
 
 class QueryAdapter:
-    def __init__(self, adapter):
-        self._adapter: DBAdapter = adapter
+    def __init__(self, adapter: DBAdapter):
+        self._tables = adapter.tables()
+        self._columns = adapter.columns()
+        self._triggers = adapter.triggers()
+        self._primary_keys = adapter.primary_keys()
+        self._references = adapter.references()
+        self._indexes = adapter.indexes()
+        self._constraints = adapter.constraints()
+        self._functions = adapter.functions()
+        self._procedures = adapter.procedures()
+        self._sequences = adapter.sequences()
 
     def tables(self) -> List[str]:
-        return self._adapter.tables()
+        return self._tables
 
     def columns(self, table_name: str) -> Dict[str, str]:
         return {
             column:ctype for (table, column, ctype)
-            in self._adapter.columns()
+            in self._columns
             if table == table_name
         }
 
     def triggers(self, table_name: str) -> Dict[str, str]:
         return {
             trigger:hook for (table, trigger, hook)
-            in self._adapter.triggers()
+            in self._triggers
             if table == table_name
         }
 
     def primary_key(self, table_name: str, column_name: str) -> bool:
-        return (table_name, column_name) in self._adapter.primary_keys()
+        return (table_name, column_name) in self._primary_keys
 
     def references(self, table_name: str, column_name: str) -> Optional[str]:
         return {
             (table, column): references for (table, column, references)
-            in self._adapter.references()
+            in self._references
         }.get((table_name, column_name))
 
     def index(self, table_name: str, column_name: str) -> Optional[str]:
-        for table, column, index in self._adapter.indexes():
+        for table, column, index in self._indexes:
             if table == table_name and column == column_name:
                 return index
         return None
@@ -40,15 +49,15 @@ class QueryAdapter:
     def constraints(self, table_name: str, column_name: str) -> Dict[str, str]:
         return {
             constraint:ctype for (table, column, constraint, ctype)
-            in self._adapter.constraints()
+            in self._constraints
             if table == table_name and column == column_name
         }
 
     def functions(self) -> List[str]:
-        return self._adapter.functions()
+        return self._functions
 
     def procedures(self) -> List[str]:
-        return self._adapter.procedures()
+        return self._procedures
 
     def sequences(self) -> List[str]:
-        return self._adapter.sequences()
+        return self._sequences
