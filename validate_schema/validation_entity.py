fe61218e75f6605dcfdaa9db81ed43f0cb136a3b
MxBromelia <ruanmacambira@gmail.com>
2020-10-02 04:47:28 -0300

    docstrings em validation_entity

--- validate_schema/validation_entity.py
+++ validate_schema/validation_entity.py
@@ -1,8 +1,12 @@
+"""Functions that presents relevant information
+about an schema entity regarding validations -
+namely: whether it needs validations and the name it will appear on the report"""
 from functools import singledispatch
 from .schema import Entity, Table, TableEntity, ColumnEntity
 
 @singledispatch
 def needs_validation(entity, _ignore_tables: list):
+    """Check if entity should or not be validated, considering the ignore-tables configuration"""
     raise TypeError("This method does not accept instances other than Entity")
 
 @needs_validation.register
@@ -19,6 +23,7 @@ def _(table_entity: TableEntity, ignore_tables: list):
 
 @singledispatch
 def canonical_name(entity):
+    """The entity identifier on the report"""
     raise NotImplementedError
 
 @canonical_name.register #type: ignore
