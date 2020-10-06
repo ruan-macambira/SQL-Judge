a793e0eebabc4bded6e156fc0c84f7d0952ca254
MxBromelia <ruanmacambira@gmail.com>
2020-10-05 22:00:22 -0300

    mais testes (falhando)

--- integration_test/test_main.py
+++ integration_test/test_main.py
@@ -1,13 +1,32 @@
+# pylint: disable=C
 import sys
-import pytest
 from sql_judge.__main__ import sql_judge
+from pytest import mark
 
 def setup_module():
     sys.path.append('integration_test/adapters')
     sys.path.append('integration_test/validations')
 
-@pytest.mark.integration_test
+@mark.integration_test
 def test_main():
     config = ['integration_test/configs/config.json']
     expected_output = []
     assert sql_judge(config) == expected_output
+
+@mark.xfail
+@mark.integration_test
+def test_empty_configuration():
+    config = ['integration_test/configs/empty_config.json']
+    assert sql_judge(config) == []
+
+@mark.xfail
+@mark.integration_test
+def test_invalid_configuration():
+    config = ['integration_test/configs/invalid_config.json']
+    assert sql_judge(config) == []
+
+@mark.xfail
+@mark.integration_test
+def test_non_existent_file():
+    config = ['integration_test/configs/nonexistent_config.json']
+    assert sql_judge(config) == []
