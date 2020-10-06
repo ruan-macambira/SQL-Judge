66513fb061f3f0e40726886d27c3ef33e0046f7e
MxBromelia <ruanmacambira@gmail.com>
2020-10-05 23:14:03 -0300

    remove nomenclatura "padrão" das configurações padrões. Configurações sem módulos explícitos afgora são considerados inválidos.

--- integration_test/build_configuration_integration_test.py
+++ integration_test/build_configuration_integration_test.py
@@ -1,5 +1,6 @@
 # pylint: disable=missing-module-docstring
 # pylint: disable=missing-function-docstring
+import json
 import pkg_resources
 from sql_judge.parse_configuration.build_configuration import ConfigurationBuilder
 
@@ -8,7 +9,16 @@ def test_build_configuration_loads_adapter_instance(build_configuration_builder,
         adapter_module='adapter', adapter_class='Adapter'
     ).build().connection == adapter_module.Adapter()
 
-def test_default_configuration_is_a_valid_json_config():
-    json_str = pkg_resources.resource_string(
-        'sql_judge.parse_configuration', 'default_configuration.json')
+def test_default_configuration_is_a_valid_json_config_except_for_user_defined_modules():
+    json_obj = json.loads(pkg_resources.resource_string(
+        'sql_judge.parse_configuration', 'default_configuration.json'))
+    json_obj.update({
+        'adapter': {
+            'module': 'adapter',
+            'class': 'Adapter'
+        }, 'validations': {
+            'module': 'validations'
+        }
+    })
+    json_str = json.dumps(json_obj)
     assert ConfigurationBuilder.from_json(json_str).is_valid() is True
