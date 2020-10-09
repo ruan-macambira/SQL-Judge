320ca85947a01982e6ddcf41192ea0b2d3f922ea
MxBromelia <ruanmacambira@gmail.com>
2020-10-09 12:19:09 -0300

    unificação de testes unitário e de integração

--- test/integration/test_main.py
+++ test/integration/test_main.py
@@ -3,30 +3,32 @@ import sys
 from sql_judge.__main__ import sql_judge
 from pytest import mark
 
+BASEPATH = 'test/integration/'
+
 def setup_module():
-    sys.path.append('integration_test/adapters')
-    sys.path.append('integration_test/validations')
+    sys.path.append(BASEPATH + 'adapters')
+    sys.path.append(BASEPATH + 'validations')
 
 @mark.integration_test
 def test_main():
-    config = ['integration_test/configs/config.json']
+    config = [BASEPATH + 'configs/config.json']
     expected_output = []
     assert sql_judge(config) == expected_output
 
 @mark.xfail
 @mark.integration_test
 def test_empty_configuration():
-    config = ['integration_test/configs/empty_config.json']
+    config = [BASEPATH + 'configs/empty_config.json']
     assert sql_judge(config) == []
 
 @mark.xfail
 @mark.integration_test
 def test_invalid_configuration():
-    config = ['integration_test/configs/invalid_config.json']
+    config = [BASEPATH + 'configs/invalid_config.json']
     assert sql_judge(config) == []
 
 @mark.xfail
 @mark.integration_test
 def test_non_existent_file():
-    config = ['integration_test/configs/nonexistent_config.json']
+    config = [BASEPATH + 'configs/nonexistent_config.json']
     assert sql_judge(config) == []
